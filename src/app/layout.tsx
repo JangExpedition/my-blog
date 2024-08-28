cb309f080ad1bb7d14f5968918d22e8d044121c4
DESKTOP-5NFJG1T\JAY <jang_away@j-iot.co.kr>
2024-08-28 17:34:30 +0900

    dark 모드 기능 구현

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
+import DarkThemeProvider from "@/components/dark-theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,12 +22,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header />
-        <main>
-          <div className="max-w-[900px] mt-[60px] mx-auto min-h-[calc(100vh-60px)]">
-            {children}
-          </div>
-        </main>
+        <DarkThemeProvider>
+          <Header />
+          <main>
+            <div className="max-w-[900px] mt-[60px] mx-auto min-h-[calc(100vh-60px)]">
+              {children}
+            </div>
+          </main>
+        </DarkThemeProvider>
       </body>
     </html>
   );
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,14 +1,20 @@
 import Link from "next/link";
+import ThemeButton from "./theme-button";
 
 export default function Header() {
   return (
-    <header className="w-full h-[60px] border-b-[1px] fixed top-0 z-50 bg-white">
+    <header className="w-full h-[60px] border-b-[1px] fixed top-0 z-50 bg-white dark:bg-black">
       <div className="w-[92%] h-full flex justify-between items-center mx-auto">
         <Link href={"/"}>
           <p className="text-[24px] font-[700] italic hover:underline">
             Tazoal.
           </p>
         </Link>
+        <ul>
+          <li>
+            <ThemeButton />
+          </li>
+        </ul>
       </div>
     </header>
   );
