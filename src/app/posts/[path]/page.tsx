d13a722f1011d39f95a5685931abe2952833c576
DESKTOP-5NFJG1T\JAY <jang_away@j-iot.co.kr>
2024-08-28 14:59:02 +0900

    Giscus 삭제

--- src/app/posts/[path]/page.tsx
+++ src/app/posts/[path]/page.tsx
@@ -2,7 +2,6 @@ import { getPostBySlug } from "@/lib/api";
 import { notFound } from "next/navigation";
 import PostHeader from "@/components/post-header";
 import PostBody from "@/components/post-body";
-import Giscus from "@/components/giscus";
 
 export default async function Post({ params }: { params: { path: string } }) {
   const post = getPostBySlug(params.path);
@@ -17,7 +16,6 @@ export default async function Post({ params }: { params: { path: string } }) {
         date={post.date}
       />
       <PostBody content={post.content} />
-      <Giscus />
     </div>
   );
 }
