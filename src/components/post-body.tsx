5a27021fadc19d177994e8699667ccc9ab4a5d9a
DESKTOP-5NFJG1T\JAY <jang_away@j-iot.co.kr>
2024-08-29 17:21:48 +0900

    rehype plugin 추가, remark-gfm 추가

--- src/components/post-body.tsx
+++ src/components/post-body.tsx
@@ -1,68 +1,28 @@
 import ReactMarkdown from "react-markdown";
-import Image from "next/image";
-import SyntaxHighlighter from "react-syntax-highlighter/dist/esm/default-highlight";
-import { dark } from "react-syntax-highlighter/dist/esm/styles/hljs";
-
-const CustomComponents = {
-  h1({ ...props }) {
-    return (
-      <h1 className="text-4xl mt-16 mb-4 leading-snug">{props.children}</h1>
-    );
-  },
-  h2({ ...props }) {
-    return (
-      <h2 className="text-3xl mt-12 mb-4 leading-snug">{props.children}</h2>
-    );
-  },
-  h3({ ...props }) {
-    return (
-      <h3 className="text-2xl mt-8 mb-4 leading-snug">{props.children}</h3>
-    );
-  },
-  code({ ...props }) {
-    return (
-      <SyntaxHighlighter
-        style={dark}
-        PreTag="div"
-        {...props}
-        className="my-4 rounded-lg"
-      >
-        {String(props.children).replace(/\n$/, "")}
-      </SyntaxHighlighter>
-    );
-  },
-  img({ ...props }) {
-    const { src, alt } = props.node.properties;
-    if (!src) return;
-    return (
-      <Image
-        src={src}
-        alt={alt}
-        width={900}
-        height={100}
-        className="my-4 w-full rounded-xl"
-      />
-    );
-  },
-  ul({ ...props }) {
-    return (
-      <ul className="list-disc list-inside my-4 ml-4">{props.children}</ul>
-    );
-  },
-  ol({ ...props }) {
-    return (
-      <ol className="list-decimal list-inside my-4 ml-4">{props.children}</ol>
-    );
-  },
-  li({ ...props }) {
-    return <li className="my-2">{props.children}</li>;
-  },
-};
+import remarkGfm from "remark-gfm";
+import rehypeHighlight from "rehype-highlight";
+import rehypeSlug from "rehype-slug";
+import rehypeAutolink from "rehype-autolink-headings";
+import rehypeRaw from "rehype-raw";
+import rehypeKatex from "rehype-katex";
+import rehypeExternalLinks from "rehype-external-links";
 
 export default function PostBody({ content }: { content: string }) {
   return (
-    <div className="w-full mt-8">
-      <ReactMarkdown components={CustomComponents}>{content}</ReactMarkdown>
+    <div className="w-full mt-8 prose">
+      <ReactMarkdown
+        remarkPlugins={[remarkGfm]}
+        rehypePlugins={[
+          rehypeHighlight,
+          rehypeSlug,
+          rehypeAutolink,
+          rehypeRaw,
+          rehypeKatex,
+          rehypeExternalLinks,
+        ]}
+      >
+        {content}
+      </ReactMarkdown>
     </div>
   );
 }
