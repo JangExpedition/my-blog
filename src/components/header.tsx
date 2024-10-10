c33ca6646e19abd28da7f801fdce986f459e71a0
DESKTOP-TUUFILQ\jang_ <jang_away@j-iot.co.kr>
2024-10-10 17:07:36 +0900

    header에 효과 추가

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,34 @@
+"use client";
 import Link from "next/link";
 import ThemeButton from "./theme-button";
+import { useEffect, useState } from "react";
 
 export default function Header() {
+  const [scrollProgress, setScrollProgress] = useState(0);
+  const borderStyle = {
+    background: `linear-gradient(to right, rgba(29, 78, 216, 1) ${scrollProgress}%, #e5e7eb ${scrollProgress}%)`,
+    height: "1px",
+    width: "100%",
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+      const windowHeight =
+        document.documentElement.scrollHeight - window.innerHeight;
+      const scrollPercent = (scrollTop / windowHeight) * 100;
+      setScrollProgress(scrollPercent);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <header className="w-full h-[60px] border-b-[1px] fixed top-0 z-50 bg-white dark:bg-black">
+    <header className="w-full h-[60px] fixed top-0 z-50 bg-white dark:bg-black">
       <div className="w-[92%] h-full flex justify-between items-center mx-auto">
         <Link href={"/"}>
           <p className="text-[24px] font-[700] italic hover:underline">
@@ -16,6 +41,7 @@ export default function Header() {
           </li>
         </ul>
       </div>
+      <div className="absolute bottom-0 left-0" style={borderStyle}></div>
     </header>
   );
 }
